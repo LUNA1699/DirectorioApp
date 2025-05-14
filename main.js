08fd05b3f243a6bbf0ea8326f2fe2426de9e26ab
LUNA1699 <miguel_luna2716@outlook.com>
2025-05-14 11:31:51 -0600

    ACTUALIZACION-CONSUMO DE API

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hola");
+//En js tenemos las funciones de tipo flecha//
+//Utilizaremos fetch es un metodo para consumir API//
+
+//funcion flecha//
+const consumirApi=()=> {
+fetch("https://jsonplaceholder.typicode.com/users") 
+//convertimos la respuesta a tipo JSON//
+.then ((responde) =>responde.json())
+// los datos los vamos a mandar a la consola//
+.then((laDataDeJsonPlaceHolder) =>console.log (laDataDeJsonPlaceHolder))
+//Descubrir que hacer en caso de que no me responda//
+.catch ((error)=> console.log(error));
+};
+
+consumirApi();
